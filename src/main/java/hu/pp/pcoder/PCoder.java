81ec5e1f1bb5ef8ee996f1f46d1b412d1060c410
P Péter <patakipeter97@yahoo.hu>
2023-07-04 20:21:00 +0200

    Initial commit

---
+++ src/main/java/hu/pp/pcoder/PCoder.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hu.pp.pcoder;
+
+/**
+ *
+ * @author patakip
+ */
+public class PCoder {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
