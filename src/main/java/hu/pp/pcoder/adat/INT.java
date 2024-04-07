1a328abd46f43cec3a83879a091646c0af92e8fe
P Péter <patakipeter97@yahoo.hu>
2024-04-07 14:16:31 +0200

    jav

--- src/main/java/hu/pp/pcoder/adat/INT.java
+++ src/main/java/hu/pp/pcoder/adat/INT.java
@@ -13,71 +13,95 @@ public class INT {
     /**
      * Integer String --> Hexadecimális string (formázott)
      *
-     * @param adatBe
+     * @param str
      */
-    public void intStringToHexString(String adatBe) {
+    public static void intStringToHexString(String str) {
         System.out.println(String.format("%02X", HEX.hexStringToByte(intToHexString(3))));
         //return String.format("%02X", String.valueOf(Integer.parseInt(adatBe, 16)));
     }
 
     /**
      * Integer --> Bináris String
+     * 
+     * A String 8 bit-re kiegészítve adja vissza az értéket
      *
-     * @param adatBe
+     * @param intSzam
      * @return
      */
-    public String intToBinString(int adatBe) {
+    public static String intToBinString(int intSzam) {
         String ret = "";
-        int hossz = Integer.toBinaryString(adatBe).length();
-        int binMaxHossz = 0;
-        for (int i = 8; i < 100000; i = i + 8) {
-            if (i - hossz >= 0) {
-                binMaxHossz = i;
+        int hossz = Integer.toBinaryString(intSzam).length();
+        int binMaxHossz, lepes = 8;
+        while (true) {
+            if (lepes - hossz >= 0) {
+                binMaxHossz = lepes;
                 break;
             }
+            lepes += 8;
         }
         String formatum = "%" + binMaxHossz + "s";
-        ret += String.format(formatum, Integer.toBinaryString(adatBe)).replace(" ", "0");
+        ret += String.format(formatum, Integer.toBinaryString(intSzam)).replace(" ", "0");
+        return ret;
+    }
+
+    /**
+     * Integer --> Bináris String
+     *
+     * @param intSzam
+     * @param kitoltes hány bit-re legyen kiegészítve
+     * @return
+     */
+    public static String intToBinString(int intSzam, int kitoltes) {
+        String ret = "";        
+        String formatum = "%" + kitoltes + "s";
+        ret += String.format(formatum, Integer.toBinaryString(intSzam)).replace(" ", "0");
+
         return ret;
     }
 
     /**
      * Integer --> Hexadecimális String (formázott)
      *
-     * @param adatBe
+     * @param intSzam
      * @return
      */
-    public static String intToHexStringFormazott(int adatBe) {
-        return String.format("%02X", intToByte(adatBe));
+    public static String intToHexStringFormazott(int intSzam) {
+        return String.format("%02X", intToByte(intSzam));
     }
 
     /**
      * Integer --> Hexadecimális String (formázatlan)
      *
-     * @param adatBe
+     * @param intSzam
      * @return
      */
-    public static String intToHexString(int adatBe) {
-        return Integer.toHexString(adatBe);
+    public static String intToHexString(int intSzam) {
+        String hexaStr = Integer.toHexString(intSzam);
+        int hLen = hexaStr.length();
+        if(hLen % 2 !=0){
+            return "0" + hexaStr;
+        } else{
+            return hexaStr;
+        }
     }
 
     /**
      * Integer --> Byte tömb
      *
-     * @param adatBe
+     * @param intSzam
      * @return
      */
-    public static byte[] intToByteArray(int adatBe) {
-        return HEX.hexStringToByteArray(intToHexString(adatBe));
+    public static byte[] intToByteArray(int intSzam) {
+        return HEX.hexStringToByteArray(intToHexString(intSzam));
     }
 
     /**
      * Integer --> Byte
      *
-     * @param adatBe
+     * @param intSzam
      * @return
      */
-    static byte intToByte(int adatBe) {
-        return (byte) adatBe;
+    public static byte intToByte(int intSzam) {
+        return (byte) intSzam;
     }
 }
